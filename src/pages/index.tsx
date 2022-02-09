d0c957f8eeba196d69d8a5e17803f8561018949e
Yuri Baza <yuribaza2321@gmail.com>
2022-02-09 19:02:49 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@chakra-ui/react'
+import type { NextPage } from 'next'
+const Home: NextPage = () => {
+  return (
+    <div className="w-screen h-screen bg-black flex items-center justify-center">
+      <Box w='20rem' h='20rem' bg='white' borderRadius='10rem'>
+        
+      </Box>
+    </div>
+  )
+}
+
+export default Home
